a25bca3267a01b2b8c20b53d47977f2d11715bfd
jawadkhanniazi <jawadkhan31849@gmail.com>
2020-06-22 03:14:25 +0500

    port changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+var port=Number(process.env.PORT || 3000);
 
 // Set public folder as root
 app.use(express.static('public'));
